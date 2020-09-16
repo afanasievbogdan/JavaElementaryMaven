2ae975a4c6e1c66ab93fe451ad7e6ad8facbfbc2
afanasievbogdan <lee.vong123@gmail.com>
2020-09-16 21:54:01 +0300

    HomeWork 17 upd commit

--- src/main/java/HomeWork17/Convertor.java
+++ src/main/java/HomeWork17/Convertor.java
@@ -6,22 +6,24 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Convertor {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите абсолютный путь к json или yaml фалу");
-        System.out.println("Например один из этих :\n" +
-                "C:\\\\Users\\\\MSI\\\\IdeaProjects\\\\JavaElementaryMaven\\\\src\\\\main\\\\java\\\\HomeWork17\\\\data.json\n" +
-                "C:\\\\Users\\\\MSI\\\\IdeaProjects\\\\JavaElementaryMaven\\\\src\\\\main\\\\java\\\\HomeWork17\\\\data.yaml");
-        String inputFilePath = scanner.nextLine();
+        String inputFilePath = args[0];
 
         ObjectMapper mapperForJson = new ObjectMapper(new JsonFactory());
         ObjectMapper mapperForYaml = new ObjectMapper(new YAMLFactory());
 
         if (inputFilePath.substring(inputFilePath.lastIndexOf(".")).equals(".json")){
             File jsonFile = new File(inputFilePath);
+
+            if (!jsonFile.exists()){
+                System.out.println("File does not exist");
+                System.exit(-1);
+            }else if (jsonFile.isDirectory()){
+                System.out.println("This is directory, not file");
+                System.exit(-1);
+            }
 
             String yamlFileName = inputFilePath.replace(".json", ".yaml");
             File jsonToYamlFile = new File(yamlFileName);
@@ -31,6 +33,15 @@ public class Convertor {
             mapperForYaml.writeValue(jsonToYamlFile, personFromJson);
         }else if (inputFilePath.substring(inputFilePath.lastIndexOf(".")).equals(".yaml")){
             File yamlFile = new File(inputFilePath);
+
+            if (!yamlFile.exists()){
+                System.out.println("File does not exist");
+                System.exit(-1);
+            }else if (yamlFile.isDirectory()){
+                System.out.println("This is directory, not file");
+                System.exit(-1);
+            }
+
             String jsonFileName = inputFilePath.replace(".yaml", ".json");
             File yamlToJsonFile = new File(jsonFileName);
 
@@ -40,11 +51,3 @@ public class Convertor {
         }else System.out.println("Error! Incorrect file path!");
     }
 }
-
-                /* FOR TESTS */
-
-//        jsonFile C:\\Users\\MSI\\IdeaProjects\\JavaElementaryMaven\\src\\main\\java\\HomeWork17\\data.json
-
-//        yamlFile C:\\Users\\MSI\\IdeaProjects\\JavaElementaryMaven\\src\\main\\java\\HomeWork17\\data.yaml
-
-                /* FOR TESTS */
